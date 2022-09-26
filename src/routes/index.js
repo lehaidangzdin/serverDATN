9ed457dab308b658160b8a043b947741e2a0bb71
Le Hai Dang <dang2002ns@gmail.com>
2022-09-26 11:46:27 +0700

    second commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 const Book = require("./book.routes");
 const Customer = require("./customer.routes");
-
-router.use("/api",Book);
-router.use("/api",Customer);
-
+router.get("/home", function (req, res, next) {
+  res.json({
+    user: "abc",
+    name: "asdadwq qwdq",
+  });
+});
+// router.use("/api", Book);
+// router.use("/api", Customer);
 
 module.exports = router;
