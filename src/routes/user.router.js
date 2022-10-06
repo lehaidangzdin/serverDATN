670a8ecd27528905171537f5a4b79020cb6ce207
Le Hai Dang <dang2002ns@gmail.com>
2022-10-06 15:18:52 +0700

    qwdg qwr3e1

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -3,9 +3,10 @@ const CustomerController = require("../controller/customer.controller");
 const router = express.Router();
 const userController = require("../controller/auth.controller")
 
-const auth = require("../controller/auth.controller")
+// const auth = require("../controller/auth.controller")
+const auth = require("../middleware/auth")
 
-router.post("/login", userController.login)
+router.post("/login", auth, userController.login)
 router.post("/register", userController.register)
 
 module.exports = router;
